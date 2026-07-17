f4bd7fc3e699a3b48bfb6e334f4769354ce3926d
ethanderouis <105126976+ethanderouis@users.noreply.github.com>
2026-07-17 08:26:21 -0400

    started learning some javascript

---
+++ learnJS/lesson1.js
@@ -0,0 +1,28 @@
+// CREATE VARIABLES
+let fname = "Ethan";
+console.log("Print fname below")
+console.log(`My name is ${fname}.`)
+
+let stark1, stark2, stark3;
+stark1 = "Bran";
+stark2 = "Arya";
+stark3 = "Sansa";
+
+// DATA TYPES
+// string
+let lannisterDwarf = "Tyrion";
+
+// number
+let yearOfService = 9;
+
+// boolean
+let handOfTheKing = true;
+
+// object
+const lannister = {lord:"Tywin", sigil:"lion"};
+
+// array object
+const tywinKids = ["Jaime", "Cersei", "Tyrion"];
+
+// date object
+const todaysDate = new Date("2026-07-17");
