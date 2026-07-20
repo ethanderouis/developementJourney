2dbd48a550818870d114b9fe4c8ae1e0329f87b1
ethanderouis <105126976+ethanderouis@users.noreply.github.com>
2026-07-20 12:34:40 -0400

    learned all the way to loops

--- learnJS/lesson1.js
+++ learnJS/lesson1.js
@@ -26,3 +26,102 @@ const tywinKids = ["Jaime", "Cersei", "Tyrion"];
 
 // date object
 const todaysDate = new Date("2026-07-17");
+
+// OPERATORS
+let tubberAge = 14;
+let lillieAge = 20;
+let combinedAge = tubberAge + lillieAge;
+
+/*
+
++   Addition
+-   Subtraction
+*   Multiplication
+**  Exponentiation
+/   Division
+%   Modulus (Division Remainder)
+++  Increment
+--  Decrement
+
+*/
+let lord = "Eddard Stark";
+let land = "Winterfell";
+
+let houseStark = lord + " of " + land;
+
+/*
+comparison operators
+
+==  equal to
+=== equal value and equal type
+!=  not equal
+!== not equal value or not equal type
+>   greater than   
+<   less than
+>=  greater than or equal to
+<=  less than or equal to
+
+*/
+
+let tobiOlderLillie = tubberAge > lillieAge; // false
+
+/*
+logical operators
+
+&&  logical and
+||  logical or
+!   logical not
+
+*/
+
+/*
+conditional statements
+
+if (condition1) {
+    code to execute if the condition is true
+} else if (condition2) {
+    code to execute if the condition1 is false
+} else {
+    code to execute if condition1 and condition2 is false
+}
+*/
+
+let age = 25;
+if (age >= 25) {
+    text = "You can rent a car!";
+} else {
+    text = "You cannot rent a car!";
+}
+
+/*
+ternary condition
+shorthand
+let data = condition ? expression1 : expression 2;
+let text = (age >= 25) ? "You can rent a car!" : "You cannot rent a car!";
+*/
+
+let day;
+let todayDate = new Date().getDay();
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(day);
